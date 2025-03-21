d527e3c82d64912e32cf4b368b2c42be2fed4d19
Julia Roxana Natera <103115280+roxanatera@users.noreply.github.com>
2025-03-21 12:42:52 +0100

    añadiendo imagenes

--- handler.js
+++ handler.js
@@ -1,5 +1,11 @@
 const { v4: uuidv4 } = require('uuid');
 
+
+const { SQSClient, SendMessageCommand } = require ("@aws-sdk/client-sqs");
+
+// Create an SQS client
+const sqsClient = new SQSClient({ region: process.env.REGION });
+
 exports.newOrder = async (event) => {
     const orderId = uuidv4();
     console.log(orderId);
@@ -18,6 +24,8 @@ exports.newOrder = async (event) => {
     console.log(orderDetails)
   
     const order = {orderId, ...orderDetails}
+    const PENDING_ORDERS_QUEUE_URL = process.env.PENDING_ORDERS_QUEUE;
+    await sendMessageToSQS(order, PENDING_ORDERS_QUEUE_URL);
   
     return {
       statusCode: 200,
@@ -54,3 +62,44 @@ exports.getOrder = async (event) => {
   };
 };
 
+exports.preOrder = async (event) => {
+  console.log("Evento recibido desde SQS:", event);
+
+  return;
+};
+
+exports.sendOrder = async (event) => {
+  console.log(event);
+
+  const order = {
+    orderId: event.orderId,
+    burguer: event.burguer,
+    customerId: event.burguer
+  }
+
+  const ORDERS_TO_SEND_QUEUE_URL = process.env.ORDERS_TO_SEND_QUEUE
+
+  await sendMessageToSQS(order, ORDERS_TO_SEND_QUEUE_URL);
+
+  return;
+}
+
+async function sendMessageToSQS(message, queueURL) {
+
+  const params = {
+    QueueUrl: process.env.PENDING_ORDERS_QUEUE,
+    MessageBody: JSON.stringify(message)
+  };
+
+  console.log(params);
+
+  try {
+    const command = new SendMessageCommand(params);
+    const data = await sqsClient.send(command);
+    console.log("Message sent successfully:", data.MessageId);
+    return data;
+  } catch (error) {
+    console.error("Error sending message:", error);
+    throw error;
+  }
+}
